e5ebba701a72d56173b9110ee202fd939a52d2c9
Ronny Hildebrandt <49238560+hilderonny@users.noreply.github.com>
2024-06-19 18:56:24 +0200

    Cleanup

--- mediatranscriber.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-git pull
-python\python mediatranscriber.py --apiurl http://192.168.0.152:30000/api/ --whisperpath \faster-whisper --whispermodel large-v2 --usegpu --sharepath X:\data\forensictaskbridge\input
--- texttranslator.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-git pull
-python\python texttranslator.py --apiurl http://192.168.0.152:30000/api/ --argospath D:\argos-translate --usegpu
